0ec67bfb9e2f6e7d620580f79e88c8de08bc43e2
Schchl <chloe34350@gmail.com>
2026-09-08 09:06:27 +0200

    Ajouts vidéos

--- data/videos.js
+++ data/videos.js
@@ -22,6 +22,66 @@ const VIDEOS = [
     date: "05/06/2026",
     anecdote: "",
     drive: "https://drive.google.com/file/d/1r0Q2tNnY_PJbppUNnO_ow2BBVimx-Voj/view?usp=sharing"
+  },
+  {
+    title: "Aladin Maison pour tous Voltaire",
+    date: "21/03/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1cJxPmjLeilsN1Sp9NLeNaPhsU1Tr4YxN/view?usp=sharing"
+  },
+  {
+    title: "All I Want For Christmas Is You Marché de Noël",
+    date: "13/12/2025",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1slCLk4-bbltFQmo_MOeiO4Oqp_NYhHQ0/view?usp=sharing"
+  },
+  {
+    title: "Chapi-chapo Reine des neiges MDE",
+    date: "10/02/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1p-7rxGKxzWaRTtOLjxMwFboncyUr2Wu2/view?usp=sharing"
+  },
+  {
+    title: "Chapi-chapo Reine des neiges MDE 2",
+    date: "10/02/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1C2QYvF4Gz43AsubrRJDnnjKuhUH4kmTB/view?usp=sharing"
+  },
+  {
+    title: "Indes Galantes Week-end OSEM",
+    date: "12/04/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1EK74x9TsNJdA-91dpY7gFt8Dn8PPxzd0/view?usp=sharing"
+  },
+  {
+    title: "La Truite Week-end OSEM",
+    date: "12/04/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1jKuwkNYyq5Yzjvz5gWa6iF14h-SnCENj/view?usp=sharing"
+  },
+  {
+    title: "Star Wars Concert Fac des sciences",
+    date: "16/12/2025",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1yv2rNbjOBnQTIaM0LsLms4MZKoyVftOq/view?usp=sharing"
+  },
+  {
+    title: "Tico-tico Concert MO.CO",
+    date: "13/02/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1yv2rNbjOBnQTIaM0LsLms4MZKoyVftOq/view?usp=sharing"
+  },
+  {
+    title: "Valse des fleurs Concert Fac de médecine",
+    date: "31/03/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1kpB-w97bmWwekNMvd2PWzwnXVa2ww_jc/view?usp=sharing"
+  },
+  {
+    title: "Valse des fleurs Concert Fac de médecine",
+    date: "31/03/2026",
+    anecdote: "",
+    drive: "https://drive.google.com/file/d/1kpB-w97bmWwekNMvd2PWzwnXVa2ww_jc/view?usp=sharing"
   }
 ];
 
